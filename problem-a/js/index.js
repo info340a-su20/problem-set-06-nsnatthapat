55dea2cfef3034b69cf7586d17cf392cd04b14b1
Natthapat <nsnatthapat@gmail.com>
2020-08-17 20:19:36 +0700

    finish

--- problem-a/js/index.js
+++ problem-a/js/index.js
@@ -35,6 +35,14 @@ const EXAMPLE_SEARCH_RESULTS = {results:[{
 //(e.g., `EXAMPLE_SEARCH_RESULTS.results[0]).
 
 
+function renderTrack(singleSong) {
+  let divElem = document.querySelector("#records");
+  let toAdd = document.createElement('img');
+  toAdd.src = singleSong.artworkUrl100;
+  toAdd.alt = singleSong.trackName;
+  toAdd.title = singleSong.trackName;
+  divElem.appendChild(toAdd);
+}
 
 //Define a function `renderSearchResults()` that takes in an object with a
 //`results` property containing an array of music tracks; the same format as
@@ -45,7 +53,19 @@ const EXAMPLE_SEARCH_RESULTS = {results:[{
 //
 //You can test this function by passing it the `EXAMPLE_SEARCH_RESULTS` object.
 
-
+function renderSearchResults(object) {
+  let divElem = document.querySelector("#records");
+  divElem.innerHTML = ''; 
+  let results = object.results;
+  let length = results.length;
+  if (object.results.length === 0){
+    renderError(new Error("No results found"));
+  }
+  for(let i = 0; i < length; i++) {
+    let track = results[i];
+    renderTrack(track);
+  }
+}
 
 //Now it's the time to practice using `fetch()`! First, modify the `index.html`
 //file to load the polyfills for _BOTH_ the fetch() function and Promises, so
@@ -69,7 +89,24 @@ const EXAMPLE_SEARCH_RESULTS = {results:[{
 //your favorite band (you CANNOT test it with the search button yet!)
 const URL_TEMPLATE = "https://itunes.apple.com/search?entity=song&limit=25&term={searchTerm}";
 
+function fetchTrackList(searchTerm) {
+  let url = URL_TEMPLATE.substring(0,58) + searchTerm;
+  togglerSpinner()
+  let promise1 = fetch(url);
 
+  let dataPromise = promise1.then(function(response) {
+    let dataPromise = response.json();
+    return dataPromise;
+  });
+  let rendered = dataPromise.then(renderSearchResults)
+  
+  let errorCheck = rendered.catch(function(err){
+    renderError(err);
+  });
+
+  let toggled = errorCheck.then(togglerSpinner);
+  return toggled;
+}
 
 
 //Add an event listener to the "search" button so that when it is clicked (and 
@@ -77,14 +114,19 @@ const URL_TEMPLATE = "https://itunes.apple.com/search?entity=song&limit=25&term=
 //user-entered `#searchQuery` value. Use the `preventDefault()` function to keep
 //the form from being submitted as usual (and navigating to a different page).
 
+let search = document.querySelector("#searchQuery");
+let form = document.querySelector('form');
+
+form.addEventListener('click', function(event) {
+  event.preventDefault();
+  fetchTrackList(search.value);
+});
 
 
 //Next, add some error handling to the page. Define a function `renderError()`
 //that takes in an "Error object" and displays that object's `message` property
 //on the page. Display this by creating a `<p class="alert alert-danger">` and
 //placing that alert inside the `#records` element.
-
-
 
 //Add the error handing to your program in two ways:
 //(1) Add a `.catch()` callback to the AJAX call in `fetchTrackList()` that
@@ -94,6 +136,14 @@ const URL_TEMPLATE = "https://itunes.apple.com/search?entity=song&limit=25&term=
 //    it an new Error object: `new Error("No results found")`
 //
 //You can test this error handling by trying to search with an empty query.
+
+function renderError(errorObject) {
+    let error = document.createElement('p');
+    error.textContent = errorObject.message;
+    error.classList.add("alert");
+    error.classList.add("alert-danger");
+    document.querySelector("#records").appendChild(error);
+}
 
 
 //Finally, add a "loading spinner" as user feedback in case the download takes a
@@ -107,6 +157,30 @@ const URL_TEMPLATE = "https://itunes.apple.com/search?entity=song&limit=25&term=
 //spinner (show it) BEFORE you send the AJAX request, and toggle it back off
 //after the ENTIRE request is completed (including after any error catching---
 //download the data and `catch()` the error, and `then()` show the spinner.
+
+function togglerSpinner() {
+  let spinner = document.querySelector(".fa-spinner");
+  spinner.classList.toggle("d-none");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
